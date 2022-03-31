df638165b0627110fc3a7d7c9ed0c059b341349c
Khoren Hovhannisyan <khoren@mail.ru>
2022-03-31 19:58:18 +0200

    add sript install-packages

---
+++ install-packages.R
@@ -0,0 +1,24 @@
+#get all installed packages
+install.packages()
+
+install.packages("dslabs")
+
+install.packages("tidyverse")
+
+
+install.packages("dplyr")
+
+
+install.packages("HistData")
+
+
+install.packages("rJava")
+install.packages("xlsx")
+
+install.packages("data.table")
+
+
+#geopackages
+install.packages("ggmap")
+install.packages("maps")
+install.packages("calibrate")
